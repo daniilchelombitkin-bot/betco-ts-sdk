6249f0f31cfc0d246f06cf5a4bdc4d577e39cbc7
daniilchelombitkin <daniilchelombitkin@gmail.com>
2026-03-13 17:32:47 +0400

    Add Content-Type header; fix getRegistrationStatisticsDetails params

--- src/actions/BaseAction.ts
+++ src/actions/BaseAction.ts
@@ -35,6 +35,7 @@ export abstract class BaseAction {
             headers: {
                 'Authentication': cookies['bo_sso_token'] ?? '',
                 'Cookie': buildCookieHeader(cookies),
+                'Content-Type': 'application/json',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15',
             },
             ...((['GET', 'OPTIONS', 'HEAD'].includes(method)) ? { params } : { data: params }),
